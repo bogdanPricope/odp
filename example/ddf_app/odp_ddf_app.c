12c1ac66b4d2ec99e0e61ff9ce35db625a8ad507
Bogdan Pricope <bogdan.pricope@linaro.org>
2017-10-23 11:16:25 +0300

    example: ddf_app: create custom pktio type
    Demonstrate registration with modular framework of a custom
    pktio type and interaction with ddf driver concept.
    
    Signed-off-by: Bogdan Pricope <bogdan.pricope@linaro.org>

--- example/ddf_app/odp_ddf_app.c
+++ example/ddf_app/odp_ddf_app.c
@@ -17,12 +17,21 @@
 #include <odp_drv.h>
 #include <odp/helper/odph_api.h>
 
+#define POOL_NUM_PKT           2048  /* Number of packets in packet pool */
+#define POOL_PKT_LEN           1856  /* Max packet length */
+
 int main(int argc, char *argv[])
 {
 	odp_instance_t instance;
+	odp_pktio_t pktio = ODP_PKTIO_INVALID;
+	odp_pool_param_t params;
+	odp_pool_t pool = ODP_POOL_INVALID;
 
-	(void)argc;
-	(void)argv;
+	if (argc == 1 || argc > 2) {
+		printf("Error: invalid parameter.\nUsage:\n\t%s <interface>\n",
+		       argv[0]);
+		exit(0);
+	}
 
 	EXAMPLE_DBG("Start DDF Application...\n");
 
@@ -36,6 +45,33 @@ int main(int argc, char *argv[])
 		EXAMPLE_ERR("Error: ODP local init failed.\n");
 		exit(EXIT_FAILURE);
 	}
+
+	/* Create packet pool */
+	odp_pool_param_init(&params);
+	params.pkt.seg_len = POOL_PKT_LEN;
+	params.pkt.len     = POOL_PKT_LEN;
+	params.pkt.num     = POOL_NUM_PKT;
+	params.type        = ODP_POOL_PACKET;
+
+	pool = odp_pool_create("packet_pool", &params);
+
+	if (pool == ODP_POOL_INVALID) {
+		EXAMPLE_ERR("Error: packet pool create failed.\n");
+		exit(EXIT_FAILURE);
+	}
+	odp_pool_print(pool);
+
+	/* Open pktio*/
+	pktio = odp_pktio_open(argv[1], pool, NULL);
+	if (pktio == ODP_PKTIO_INVALID)
+		EXAMPLE_ERR("Error: Failed to open pktio \"%s\".\n", argv[1]);
+
+	/* Close pktio*/
+	if (odp_pktio_close(pktio))
+		EXAMPLE_ERR("Error: Failed to close pktio \"%s\".\n", argv[1]);
+
+	if (odp_pool_destroy(pool))
+		EXAMPLE_ERR("Error: Failed to destroy packet pool\n");
 
 	/* Print ddf objects*/
 	odpdrv_print_all();
--- example/ddf_ifs/ddf_ifs_driver.c
+++ example/ddf_ifs/ddf_ifs_driver.c
@@ -7,8 +7,17 @@
 #include <stdio.h>
 #include <string.h>
 #include "odp_drv.h"
+#include <odp_packet_io_internal.h>
 #include "ddf_ifs_api.h"
 #include "ddf_ifs_driver.h"
+
+#define DDF_IFS_NAME_SIZE 50
+#define DDF_IFS_DEV_PREFIX "ddf_ifs:"
+#define DDF_IFS_DEV_PREFIX_LEN 8
+/** Packet socket using netmap mmaped rings for both Rx and Tx */
+typedef struct {
+	char dev_name[DDF_IFS_NAME_SIZE];	/**< interface name */
+} pktio_ops_ddf_ifs_data_t;
 
 static odpdrv_driver_t ddf_ifs_driver;
 static int drv_data = 12;
@@ -46,8 +55,88 @@ static int ddf_ifs_driver_remove(void)
 	return 0;
 }
 
+static int ddf_ifs_init_global(void)
+{
+	printf("%s()\n", __func__);
+	return 0;
+}
+
+static int ddf_ifs_term_global(void)
+{
+	printf("%s()\n", __func__);
+	return 0;
+}
+
+static int ddf_ifs_open(odp_pktio_t pktio ODP_UNUSED,
+			pktio_entry_t *pktio_entry,
+			const char *devname, odp_pool_t pool)
+{
+	pktio_ops_ddf_ifs_data_t *pkt_ddf_ifs = NULL;
+
+	(void)pktio;
+	(void)pool;
+
+	printf("%s(%s)\n", __func__, devname);
+	if (strncmp(devname, DDF_IFS_DEV_PREFIX, DDF_IFS_DEV_PREFIX_LEN) != 0)
+		return -1;
+
+	devname += DDF_IFS_DEV_PREFIX_LEN;
+
+	pktio_entry->s.ops_data = ODP_OPS_DATA_ALLOC(sizeof(*pkt_ddf_ifs));
+	if (odp_unlikely(pktio_entry->s.ops_data == NULL)) {
+		printf("Failed to allocate pktio_ops_ddf_ifs_data_t struct");
+		return -1;
+	}
+	pkt_ddf_ifs = pktio_entry->s.ops_data;
+
+	strcpy(pkt_ddf_ifs->dev_name, devname);
+	printf("%s() - device %s oppened!!!\n", __func__,
+	       pkt_ddf_ifs->dev_name);
+	return 0;
+}
+
+static int ddf_ifs_close(pktio_entry_t *pktio_entry)
+{
+	pktio_ops_ddf_ifs_data_t *pkt_ddf_ifs = pktio_entry->s.ops_data;
+
+	printf("%s() - device %s closed.\n", __func__, pkt_ddf_ifs->dev_name);
+
+	return ODP_OPS_DATA_FREE(pktio_entry->s.ops_data);
+}
+
+static pktio_ops_module_t ddf_ifs_pktio_ops = {
+	.base = {
+		.name = "ddf_ifs",
+		.init_local = NULL,
+		.term_local = NULL,
+		.init_global = ddf_ifs_init_global,
+		.term_global = ddf_ifs_term_global,
+	},
+	.open = ddf_ifs_open,
+	.close = ddf_ifs_close,
+	.start = NULL/*ddf_ifs_start*/,
+	.stop = NULL/*ddf_ifs_stop*/,
+	.stats = NULL/*ddf_ifs_stats*/,
+	.stats_reset = NULL/*ddf_ifs_stats_reset*/,
+	.pktin_ts_res = NULL,
+	.pktin_ts_from_ns = NULL,
+	.recv = NULL/*ddf_ifs_recv*/,
+	.send = NULL/*ddf_ifs_send*/,
+	.mtu_get = NULL/*ddf_ifs_mtu_get*/,
+	.promisc_mode_set = NULL/*ddf_ifs_promisc_mode_set*/,
+	.promisc_mode_get = NULL/*ddf_ifs_promisc_mode_get*/,
+	.mac_get = NULL/*ddf_ifs_mac_addr_get*/,
+	.link_status = NULL/*ddf_ifs_link_status*/,
+	.capability = NULL/*ddf_ifs_capability*/,
+	.config = NULL,
+	.input_queues_config = NULL/*ddf_ifs_input_queues_config*/,
+	.output_queues_config = NULL/*ddf_ifs_output_queues_config*/,
+	.print = NULL/*ddf_ifs_print*/,
+};
+
 int register_driver(void)
 {
+/* Register ddf driver*/
 	odpdrv_driver_param_t param = {
 		.name = DDF_IFS_DRV_NAME,
 		.devios = {{DDF_IFS_DEVIO_API_NAME, DDF_IFS_DEVIO_API_VER},
@@ -65,5 +154,12 @@ int register_driver(void)
 		return -1;
 	}
 
+/* Register pktio_ops module*/
+	odp_module_constructor(&ddf_ifs_pktio_ops);
+
+	odp_subsystem_register_module(pktio_ops, &ddf_ifs_pktio_ops);
+
+/* Explicit call to Init global*/
+	ddf_ifs_init_global();
 	return 0;
 }
